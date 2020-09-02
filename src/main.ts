63fa5a757fb8929ddb9f32fe30073bb4546a1b0c
Douglas Anderson <hockeybuggy@gmail.com>
2020-09-01 21:40:18 -0300

    Get the tests passing.
    This commit gets the very beginnings of real functionally working.
    
    I chose to introduce an hatch for monkey patching by exposing an
    `console` attribute on the `ProperLogger` instances. The will call
    that reference instead of the global directly allowing us to mock
    things.

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 export const LogLevels = {
   DEBUG: 10,
   INFO: 20,
@@ -8,11 +9,20 @@ export const LogLevels = {
 export class ProperLogger {
   name: string;
 
+  console: { log: (message: string) => void };
+
   static getLogger(name: string): ProperLogger {
     return new ProperLogger(name);
   }
 
   constructor(name: string) {
     this.name = name;
+    // Here we grab a reference to the `global.console` so that in tests we can
+    // patch this out.
+    this.console = global.console;
+  }
+
+  info(message: string): void {
+    this.console.log(JSON.stringify({ message }));
   }
 }
--- test/ProperLogger.test.ts
+++ test/ProperLogger.test.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-console, @typescript-eslint/no-empty-function */
+
 import { ProperLogger, LogLevels } from '..';
 
 describe('LogLevels', () => {
@@ -6,5 +8,28 @@ describe('LogLevels', () => {
     expect(LogLevels.INFO).toEqual(20);
     expect(LogLevels.WARNING).toEqual(30);
     expect(LogLevels.ERROR).toEqual(40);
+  });
+});
+
+describe('ProperLogger', () => {
+  function setupTestLogger(): ProperLogger {
+    const logger = new ProperLogger('test');
+    logger.console = {
+      log: jest.fn(),
+    };
+
+    return logger;
+  }
+
+  it('logs to the console at an info level', () => {
+    const logger = setupTestLogger();
+
+    expect(logger.console.log).not.toHaveBeenCalled();
+
+    logger.info('Logs to the console');
+
+    expect((logger.console.log as jest.Mock).mock.calls).toEqual([
+      ['{"message":"Logs to the console"}'],
+    ]);
   });
 });
